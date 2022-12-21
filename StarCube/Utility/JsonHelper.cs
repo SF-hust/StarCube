00cef24e5b97b9b21e5f4c93114723f33c86d677
Zhao Yi <1609957690@qq.com>
2022-12-21 10:55:13 +0800

    增加了 json 的 helper 类

---
+++ StarCube/Utility/JsonHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace StarCube.Utility
+{
+    public static class JsonHelper
+    {
+        public static bool TryGetBoolean(JObject json, string propertyName, out bool value)
+        {
+            if (json.TryGetValue(propertyName, out JToken? token) && token is JValue jValue && jValue.Value is bool bValue)
+            {
+                value = bValue;
+                return true;
+            }
+            value = false;
+            return false;
+        }
+
+        public static bool TryGetArray(JObject json, string propertyName, [NotNullWhen(true)] out JArray? array)
+        {
+            if (json.TryGetValue(propertyName, out JToken? token) && token is JArray arr)
+            {
+                array = arr;
+                return true;
+            }
+            array = null;
+            return false;
+        }
+    }
+}
