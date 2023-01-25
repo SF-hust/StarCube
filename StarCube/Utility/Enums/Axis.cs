be72fa5a111f6fb953489b0dfbe872786e61fb47
Zhao Yi <1609957690@qq.com>
2023-01-25 18:04:23 +0800

    添加了 Axis 和 Direction 枚举

---
+++ StarCube/Utility/Enums/Axis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarCube.Utility.Enums
+{
+    public enum Axis
+    {
+        X,
+        Z,
+        Y,
+    }
+}
---
+++ StarCube/Utility/Enums/Direction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarCube.Utility.Enums
+{
+    public enum Direction
+    {
+        North = 0x1,
+        East = 0x2,
+        South = 0x4,
+        West = 0x8,
+        Up = 0x10,
+        Down = 0x20,
+    }
+}
