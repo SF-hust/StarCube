6793260d3a6039502f22793e8c7b9cffe6755f8a
Zhao Yi <1609957690@qq.com>
2023-01-26 21:24:58 +0800

    添加了 AirBlock 以及 Block 构建的助手函数

---
+++ StarCube/Game/Block/BlockBuildHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using StarCube.Core.State;
+
+namespace StarCube.Game.Block
+{
+    public static class BlockBuildHelper
+    {
+        public static Block BuildSingleBlockState(this Block block)
+        {
+            block.StateDefinition = StateDefinition<Block, BlockState>.BuildSingle(block, BlockState.Create);
+            return block;
+        }
+
+        public static BlockStateBuildHelper StartBuildBlockState(this Block block)
+        {
+            return new BlockStateBuildHelper(block);
+        }
+    }
+
+    public class BlockStateBuildHelper
+    {
+        public readonly Block block;
+
+        private readonly StateDefinition<Block, BlockState>.Builder builder;
+
+        public BlockStateBuildHelper(Block block)
+        {
+            this.block = block;
+            builder = StateDefinition<Block, BlockState>.Builder.Create(block, BlockState.Create);
+        }
+
+        public BlockStateBuildHelper Add(StateProperty property, int index)
+        {
+            builder.AddPropertyAndDefaultIndex(property, index);
+            return this;
+        }
+
+        public Block Build()
+        {
+            block.StateDefinition = builder.Build();
+            return block;
+        }
+    }
+}
---
+++ StarCube/Game/Block/Blocks.cs
@@ -0,0 +1,18 @@
+﻿using StarCube.Utility;
+using StarCube.Resource;
+using StarCube.BootStrap;
+using StarCube.Core.Registry;
+
+namespace StarCube.Game.Block
+{
+    [ConstructInBootStrap]
+    public static class Blocks
+    {
+        public static Block Air = new Block().BuildSingleBlockState();
+
+        static Blocks()
+        {
+            Registries.BlockRegistry.Add(Air, ResourceLocation.Create(Constants.DEFAULT_NAMESPACE, "air"));
+        }
+    }
+}
