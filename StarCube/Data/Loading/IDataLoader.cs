abb4edceb282ed53520fb3b1b8dffd30232494c0
Zhao Yi <1609957690@qq.com>
2023-02-09 23:36:11 +0800

    添加了 IDataLoader 接口

---
+++ StarCube/Data/Loading/IDataLoader.cs
@@ -0,0 +1,11 @@
+﻿namespace StarCube.Data.Loading
+{
+    public interface IDataLoader
+    {
+        /// <summary>
+        /// 执行 DataLoader
+        /// </summary>
+        /// <param name="reader"></param>
+        void Run();
+    }
+}
