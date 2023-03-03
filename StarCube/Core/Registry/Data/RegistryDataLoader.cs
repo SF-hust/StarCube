0ed79506948a85db3264a36dfcf78d916a0e60bc
Zhao Yi <1609957690@qq.com>
2023-03-03 12:52:41 +0800

    添加了空的 RegistryDataLoader

---
+++ StarCube/Core/Registry/Data/RegistryDataLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using StarCube.Core.Data.Loading;
+
+namespace StarCube.Core.Registry.Data
+{
+    public class RegistryDataLoader : IDataLoader
+    {
+        public void Run(IDataProvider dataProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
