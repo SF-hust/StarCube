7adab83303d8a50c6220a6dfde0ab87fa7ae5d12
Zhao Yi <1609957690@qq.com>
2023-03-04 22:06:40 +0800

    新增了 DataReader

---
+++ StarCube/Data/Loading/IDataReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace StarCube.Data.Loading
+{
+    public interface IDataReader<T>
+        where T : class
+    {
+        public bool TryLoadData(IDataProvider dataProvider, StringID id, [NotNullWhen(true)] out T? data);
+    }
+}
