d4ca17b1fe8e6ff87379a3059eb05ca50a3ff812
Zhao Yi <1609957690@qq.com>
2023-03-09 22:05:37 +0800

    添加了 ZipDataProvider

---
+++ StarCube/Data/Provider/ZipDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarCube.Data.Provider
+{
+    public class ZipDataProvider : IDataProvider
+    {
+        void IDataProvider.EnumerateData(string registry, string directory, List<RawDataEntry> dataEntries)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IDataProvider.EnumerateDataChain(string registry, string directory, List<List<RawDataEntry>> dataEntryChains)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDataProvider.TryGetData(string modid, string registry, string directory, string entryName, out RawDataEntry dataEntry)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IDataProvider.TryGetDataChain(string modid, string registry, string directory, string entryName, List<RawDataEntry> dataEntryChain)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
