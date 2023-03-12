601511f718bf5ea990885443c0d6d5babb2412c8
Zhao Yi <1609957690@qq.com>
2023-03-12 18:09:58 +0800

    新添 BlockState 相关数据的基础类

---
+++ StarCube/Game/Block/Data/BlockStateDataBase.cs
@@ -0,0 +1,16 @@
+﻿using StarCube.Utility;
+
+namespace StarCube.Game.Block.Data
+{
+    public abstract class BlockStateDataBase : IStringID
+    {
+        StringID IStringID.ID => id;
+
+        public BlockStateDataBase(StringID id)
+        {
+            this.id = id;
+        }
+
+        public readonly StringID id;
+    }
+}
---
+++ StarCube/Game/Block/Data/BlockStatePropertyMatcher.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+
+using Newtonsoft.Json.Linq;
+
+using StarCube.Core.State.Property;
+
+namespace StarCube.Game.Block.Data
+{
+    /// <summary>
+    /// 表示一个用于与 BlockState 所含有的属性进行匹配的匹配器
+    /// </summary>
+    public readonly struct BlockStatePropertyMatcher
+    {
+        public static readonly BlockStatePropertyMatcher EMPTY = new BlockStatePropertyMatcher(new List<KeyValuePair<string, List<string>>>());
+
+        public static bool TryParseFromJson(JObject json, out BlockStatePropertyMatcher matcher)
+        {
+            matcher = BlockStatePropertyMatcher.EMPTY;
+
+            List<KeyValuePair<string, List<string>>> propertyNameToValueStrings = new List<KeyValuePair<string, List<string>>>();
+            foreach (var pair in json)
+            {
+                List<string> valueStrings = new List<string>();
+                // 为字符串，这个属性只匹配一个值
+                if (pair.Value is JValue singleValue)
+                {
+                    valueStrings.Add(singleValue.ToString());
+                }
+                // 为数组，这个属性可以匹配多个值
+                else if (pair.Value is JArray array)
+                {
+                    foreach (JToken token in array)
+                    {
+                        if (token is JValue jValue)
+                        {
+                            valueStrings.Add(jValue.ToString());
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+                // 格式错误
+                else
+                {
+                    return false;
+                }
+
+                propertyNameToValueStrings.Add(new KeyValuePair<string, List<string>>(pair.Key, valueStrings));
+            }
+
+            matcher = new BlockStatePropertyMatcher(propertyNameToValueStrings);
+            return true;
+        }
+
+
+        /// <summary>
+        /// 尝试将某个 BlockState 的属性与对应的值与这个 Matcher 进行匹配
+        /// </summary>
+        /// <param name="blockState"></param>
+        /// <returns>匹配是否成功</returns>
+        public bool Match(BlockState blockState)
+        {
+            // 必须所有的属性均匹配成功，BlockState 匹配才成功
+            foreach (KeyValuePair<string, List<string>> pair in propertyNameToValueStrings)
+            {
+                int propertyIndex = blockState.propertyList.IndexOf(pair.Key);
+                // 不存在对应名字的属性则匹配失败
+                if (propertyIndex < 0)
+                {
+                    return false;
+                }
+
+                StateProperty property = blockState.propertyList[propertyIndex].property;
+                int valueIndex = blockState.propertyList[propertyIndex].valueIndex;
+                // 对于每个属性，任一值匹配成功，则此属性匹配成功
+                bool valueMatch = false;
+                foreach (string valueString in pair.Value)
+                {
+                    if (property.ParseToIndex(valueString) == valueIndex)
+                    {
+                        valueMatch = true;
+                        break;
+                    }
+                }
+                if (!valueMatch)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public BlockStatePropertyMatcher(List<KeyValuePair<string, List<string>>> propertyNameToValueStrings)
+        {
+            this.propertyNameToValueStrings = propertyNameToValueStrings;
+        }
+
+        public readonly List<KeyValuePair<string, List<string>>> propertyNameToValueStrings;
+    }
+}
