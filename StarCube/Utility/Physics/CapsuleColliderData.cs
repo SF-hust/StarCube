94fd27fbdb52d589bab9d964033a355460627970
Zhao Yi <1609957690@qq.com>
2023-03-12 14:53:16 +0800

    添加了三种简单碰撞体数据结构

---
+++ StarCube/Utility/Physics/CapsuleColliderData.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+using StarCube.Utility.Enums;
+
+namespace StarCube.Utility.Physics
+{
+    public struct CapsuleColliderData
+    {
+        public Vector3 center;
+        public float radius;
+        public float height;
+        public Axis axis;
+    }
+}
---
+++ StarCube/Utility/Physics/SphereColliderData.cs
@@ -0,0 +1,10 @@
+﻿using System.Numerics;
+
+namespace StarCube.Utility.Physics
+{
+    public struct SphereColliderData
+    {
+        public Vector3 center;
+        public float radius;
+    }
+}
