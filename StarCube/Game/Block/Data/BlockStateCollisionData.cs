dd175163807f87fecab77c7f4fb52328bf722436
Zhao Yi <1609957690@qq.com>
2023-03-13 22:02:52 +0800

    简化了 BlockStateCollisionData 的逻辑

--- StarCube/Game/Block/Data/BlockStateCollisionData.cs
+++ StarCube/Game/Block/Data/BlockStateCollisionData.cs
@@ -26,7 +26,7 @@ namespace StarCube.Game.Block.Data
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public static BlockStateCollisionData CreateDefault(StringID id)
+        public static BlockStateCollisionData CreateSolid(StringID id)
         {
             KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry> pair = 
                 new KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry>(BlockStatePropertyMatcher.ANY, CollisionDataEntry.SOLID);
@@ -38,17 +38,25 @@ namespace StarCube.Game.Block.Data
             data = null;
             List<KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry>> matcherToEntry = new List<KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry>>();
 
-            // 是否是多部分组成的
-            if (!json.TryGetBoolean("multipart", out bool multipart))
+            bool multipart;
+            if (json.TryGetArray("variants", out JArray? matcherToEntryArray))
             {
                 multipart = false;
             }
-
-            // 获取本文件中定义的默认值
-            CollisionDataEntry defaultEntry = CollisionDataEntry.ParseFromJson(json, CollisionDataEntry.AIR);
+            else if(json.TryGetArray("multiparts", out matcherToEntryArray))
+            {
+                multipart = true;
+            }
+            else
+            {
+                multipart = false;
+                // 将整个文件作为一个 entry
+                CollisionDataEntry singleEntry = CollisionDataEntry.ParseFromJson(json);
+                matcherToEntry.Add(new KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry>(BlockStatePropertyMatcher.ANY, singleEntry));
+            }
 
             // 按顺序遍历解析出所有匹配规则与其对应的值
-            if (json.TryGetArray("match", out JArray? matcherToEntryArray))
+            if(matcherToEntryArray != null)
             {
                 foreach (JToken token in matcherToEntryArray)
                 {
@@ -58,22 +66,19 @@ namespace StarCube.Game.Block.Data
                     }
 
                     BlockStatePropertyMatcher matcher = BlockStatePropertyMatcher.ANY;
-                    if (matcherToEntryObject.TryGetJObject("properties", out JObject? matcherObject))
+                    if (matcherToEntryObject.TryGetJObject("when", out JObject? matcherObject))
                     {
-                        if(!BlockStatePropertyMatcher.TryParseFromJson(matcherObject, out matcher))
+                        if (!BlockStatePropertyMatcher.TryParseFromJson(matcherObject, out matcher))
                         {
                             return false;
                         }
                     }
 
-                    CollisionDataEntry entry = CollisionDataEntry.ParseFromJson(matcherToEntryObject, defaultEntry);
+                    CollisionDataEntry entry = CollisionDataEntry.ParseFromJson(matcherToEntryObject);
 
                     matcherToEntry.Add(new KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry>(matcher, entry));
                 }
             }
-
-            // 在最后添加一个空的匹配规则与默认值
-            matcherToEntry.Add(new KeyValuePair<BlockStatePropertyMatcher, CollisionDataEntry>(BlockStatePropertyMatcher.ANY, defaultEntry));
 
             data = new BlockStateCollisionData(id, multipart, matcherToEntry);
             return true;
@@ -81,51 +86,45 @@ namespace StarCube.Game.Block.Data
 
 
         /// <summary>
-        /// 其中包含对 CollisionData 的引用，以及旋转信息
+        /// 其中包含对 CollisionData 的引用，以及各轴上的旋转
         /// </summary>
         public readonly struct CollisionDataEntry
         {
             /// <summary>
-            /// 默认情况无碰撞，且不会要求在这里终止匹配
+            /// 无碰撞
             /// </summary>
-            public static readonly CollisionDataEntry AIR = new CollisionDataEntry(CollisionData.AIR_COLLISION_ID, false, 0, 0, 0);
+            public static readonly CollisionDataEntry AIR = new CollisionDataEntry(CollisionData.AIR_COLLISION_ID, 0, 0, 0);
 
             /// <summary>
             /// 完整方块碰撞
             /// </summary>
-            public static readonly CollisionDataEntry SOLID = new CollisionDataEntry(CollisionData.CUBE_COLLISION_ID, false, 0, 0, 0);
+            public static readonly CollisionDataEntry SOLID = new CollisionDataEntry(CollisionData.CUBE_COLLISION_ID, 0, 0, 0);
 
-            public static CollisionDataEntry ParseFromJson(JObject json, in CollisionDataEntry defaultEntry)
+            public static CollisionDataEntry ParseFromJson(JObject json)
             {
                 if(!json.TryGetStringID("collision", out StringID collisionID))
                 {
-                    collisionID = defaultEntry.collisionID;
-                }
-                // 是否要在某个 matcher 里停下是各个节点单独定义的，不从默认值继承
-                if (!json.TryGetBoolean("stop", out bool stop))
-                {
-                    stop = false;
+                    collisionID = CollisionData.AIR_COLLISION_ID;
                 }
                 if (!json.TryGetInt32("x", out int xRot))
                 {
-                    xRot = defaultEntry.xRot;
+                    xRot = 0;
                 }
                 if (!json.TryGetInt32("y", out int yRot))
                 {
-                    yRot = defaultEntry.yRot;
+                    yRot = 0;
                 }
                 if (!json.TryGetInt32("z", out int zRot))
                 {
-                    zRot = defaultEntry.zRot;
+                    zRot = 0;
                 }
 
-                return new CollisionDataEntry(collisionID, stop, xRot, yRot, zRot);
+                return new CollisionDataEntry(collisionID, xRot, yRot, zRot);
             }
 
-            public CollisionDataEntry(StringID collisionID, bool stop, int xRot, int yRot, int zRot)
+            public CollisionDataEntry(StringID collisionID, int xRot, int yRot, int zRot)
             {
                 this.collisionID = collisionID;
-                this.stop = stop;
                 this.xRot = xRot;
                 this.yRot = yRot;
                 this.zRot = zRot;
@@ -135,7 +134,6 @@ namespace StarCube.Game.Block.Data
             /// <summary>
             /// 匹配到此项时是否要停止匹配
             /// </summary>
-            public readonly bool stop;
             public readonly int xRot;
             public readonly int yRot;
             public readonly int zRot;
@@ -161,8 +159,8 @@ namespace StarCube.Game.Block.Data
                 if(pair.Key.Match(blockState))
                 {
                     matchingEntries.Add(pair.Value);
-                    // 如果没有 multipart 标记，或此项要求在此停下，则停止匹配
-                    if (!isMultipart || pair.Value.stop)
+                    // 如果没有 multipart 标记则停止匹配
+                    if (!isMultipart)
                     {
                         break;
                     }
