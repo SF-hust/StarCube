8d6020bd83675c12bef9ac56e8c65a62a2d1b302
Zhao Yi <1609957690@qq.com>
2023-03-14 20:25:42 +0800

    添加了 TomlHelper

---
+++ StarCube/Utility/TomlHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+using Nett;
+
+namespace StarCube.Utility
+{
+    public static class TomlHelper
+    {
+        public static bool TryReadFromStream(Stream stream, long length, [NotNullWhen(true)] out TomlTable? toml)
+        {
+            bool sucess = false;
+            try
+            {
+                toml = Toml.ReadStream(stream);
+                sucess = true;
+            }
+            catch(Exception)
+            {
+                toml = null;
+            }
+
+            return sucess;
+        }
+    }
+}
