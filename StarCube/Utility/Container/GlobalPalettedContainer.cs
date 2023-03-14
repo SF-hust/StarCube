0bd652148384aebe3a052572bed8c8589c6622f3
Zhao Yi <1609957690@qq.com>
2023-03-14 13:19:49 +0800

    新添 GlobalPalettedContainer

---
+++ StarCube/Utility/Container/GlobalPalettedContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StarCube.Utility.Container
+{
+    public class GlobalPalettedContainer<T> : IPalettedContainer<T>
+        where T : class
+    {
+        public T this[int index]
+        {
+            get => idMap.ValueFor(data[index]);
+            set => data[index] = idMap.IdFor(value);
+        }
+
+        public int Length => data.Length;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (from id in data select idMap.ValueFor(id)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public GlobalPalettedContainer(IIdMap<T> idMap, int size)
+        {
+            this.idMap = idMap;
+            data = new int[size];
+        }
+
+        public int[] data;
+
+        private readonly IIdMap<T> idMap;
+    }
+}
