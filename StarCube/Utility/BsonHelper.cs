62650c2bc289efefa91b9652850557efd3dd15c4
Zhao Yi <1609957690@qq.com>
2023-03-14 15:34:05 +0800

    移除了 MongoDB.Bson

--- StarCube/Utility/BsonHelper.cs
+++ StarCube/Utility/BsonHelper.cs
@@ -1,19 +1,15 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
-using MongoDB.Bson;
-using MongoDB.Bson.IO;
-using MongoDB.Bson.Serialization;
-
 namespace StarCube.Utility
 {
     public static class BsonHelper
     {
-        public static bool TryReadFromStreamSync(FileStream stream, [NotNullWhen(true)] out BsonDocument? bson)
-        {
-            RawBsonDocument rawBson = new RawBsonDocument(new BinaryReader(stream).ReadBytes((int)stream.Length));
-            bson = rawBson.ToBsonDocument();
-            return true;
-        }
+        //public static bool TryReadFromStreamSync(FileStream stream, [NotNullWhen(true)] out BsonDocument? bson)
+        //{
+        //    RawBsonDocument rawBson = new RawBsonDocument(new BinaryReader(stream).ReadBytes((int)stream.Length));
+        //    bson = rawBson.ToBsonDocument();
+        //    return true;
+        //}
     }
 }
