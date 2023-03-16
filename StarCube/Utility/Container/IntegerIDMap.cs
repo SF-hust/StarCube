f26d149fc7ca3737a0395282e2fd5e28f8dc24af
Zhao Yi <1609957690@qq.com>
2023-03-16 21:03:58 +0800

    添加了 IntegerIDMap

---
+++ StarCube/Utility/Container/IntegerIDMap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarCube.Utility.Container
+{
+    public class IntegerIDMap<T> : IIdMap<T>
+        where T : class, IIntegerID
+    {
+        public int Count => values.Count;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        public int IdFor(T value)
+        {
+            return value.IntegerID;
+        }
+
+        public T ValueFor(int id)
+        {
+            return values[id];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IntegerIDMap(IEnumerable<T> values)
+        {
+            this.values = values.ToList();
+        }
+
+        private readonly List<T> values;
+    }
+}
