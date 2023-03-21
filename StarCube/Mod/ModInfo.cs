a51cbf6d55ede5df7652a6c1179ff9d51ce89ffc
Zhao Yi <1609957690@qq.com>
2023-03-21 18:15:02 +0800

    实验性地写了些东西

--- StarCube/Mod/ModInfo.cs
+++ StarCube/Mod/ModInfo.cs
@@ -1,23 +1,24 @@
-﻿using System.Reflection;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Reflection;
 
 namespace StarCube.Mod
 {
     public class ModInfo
     {
-
         public readonly string modid;
 
         public readonly string path;
 
-        public readonly Assembly assembly;
+        public readonly ImmutableArray<Assembly> assemblies;
 
         public readonly IMod modClass;
 
-        internal ModInfo(string modid, string path, Assembly assembly, IMod modClass)
+        internal ModInfo(string modid, string path, IEnumerable<Assembly> assemblies, IMod modClass)
         {
             this.modid = modid;
             this.path = path;
-            this.assembly = assembly;
+            this.assemblies = assemblies.ToImmutableArray();
             this.modClass = modClass;
         }
     }
--- StarCube/Mod/ModLoader.cs
+++ StarCube/Mod/ModLoader.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection;
 using System.Text;
 
 namespace StarCube.Mod
 {
     public class ModLoader
     {
+        public static void LoadModAssemblyInZip(string zipPath)
+        {
+            ZipArchive zip = ZipFile.OpenRead(zipPath);
+
+            List<string> tempFilenames = new List<string>();
+
+            foreach (var entry in zip.Entries)
+            {
+                if (entry.FullName.StartsWith("libs/", StringComparison.OrdinalIgnoreCase) && entry.FullName.EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
+                {
+                    string temp = Path.GetTempFileName();
+                    entry.ExtractToFile(temp, true);
+                    tempFilenames.Add(temp);
+                }
+            }
+        }
     }
 }
