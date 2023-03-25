69d0674fca1087fc09d98414bb3f1d3dce252520
Zhao Yi <1609957690@qq.com>
2023-03-25 23:16:10 +0800

    添加了游戏存档相关类

---
+++ StarCube/Data/Saves/GameSaves.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+using LiteDB;
+
+namespace StarCube.Data.Saves
+{
+    public sealed class GameSaves
+    {
+        public LiteDatabase GetOrCreateDB(string path)
+        {
+            string fullPath = GetFullPath(path);
+            return GetOrCreateDB(fullPath);
+        }
+
+        public bool TryGetDB(string path, [NotNullWhen(true)] out LiteDatabase? database)
+        {
+            if(pathToDataBase.TryGetValue(path, out database))
+            {
+                return true;
+            }
+
+            string fullPath = GetFullPath(path);
+            if (File.Exists(fullPath))
+            {
+                database = OpenOrCreate(fullPath);
+                return true;
+            }
+
+            database = null;
+            return false;
+        }
+
+        public bool TryCreateDB(string path, [NotNullWhen(true)] out LiteDatabase? database)
+        {
+            database = null;
+
+            if(pathToDataBase.ContainsKey(path))
+            {
+                return false;
+            }
+
+            string fullPath = GetFullPath(path);
+            if (File.Exists(fullPath))
+            {
+                return false;
+            }
+
+            database = OpenOrCreate(fullPath);
+            pathToDataBase.Add(path, database);
+            return true;
+        }
+
+        private string GetFullPath(string path)
+        {
+            if (relativeToFullPath.TryGetValue(path, out string fullPath))
+            {
+                return fullPath;
+            }
+
+            fullPath = Path.Combine(directoryPath, path.Replace('/', Path.PathSeparator));
+            return fullPath;
+        }
+
+        private LiteDatabase OpenOrCreate(string fullPath)
+        {
+            ConnectionString connectionString = new ConnectionString()
+            {
+                Filename = fullPath,
+                Collation = Collation.Binary,
+            };
+            return new LiteDatabase(connectionString);
+        }
+
+        public void Flush()
+        {
+        }
+
+        public GameSaves(string name, string directoryPath)
+        {
+            this.name = name;
+            this.directoryPath = directoryPath;
+            relativeToFullPath = new Dictionary<string, string>();
+            pathToDataBase = new Dictionary<string, LiteDatabase>();
+        }
+
+        public readonly string name;
+
+        public readonly string directoryPath;
+
+        private readonly Dictionary<string, string> relativeToFullPath;
+
+        private readonly Dictionary<string, LiteDatabase> pathToDataBase;
+    }
+}
---
+++ StarCube/Data/Saves/GameSavesManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace StarCube.Data.Saves
+{
+    public sealed class GameSavesManager
+    {
+        private static readonly char[] invalidFileNameChar = Path.GetInvalidFileNameChars();
+
+        public bool Contains(string name)
+        {
+            Update();
+
+            return nameToGameSaves.ContainsKey(name);
+        }
+
+        public bool TryCreateGameSaves(string name, [NotNullWhen(true)] out GameSaves? gameSaves)
+        {
+            Update();
+
+            gameSaves = null;
+
+            if (name.IndexOfAny(invalidFileNameChar) != -1)
+            {
+                return false;
+            }
+
+            if(nameToGameSaves.ContainsKey(name))
+            {
+                return false;
+            }
+
+            string path = Path.Combine(rootSavePath, name);
+            Directory.CreateDirectory(name);
+            gameSaves = new GameSaves(name, path);
+
+            nameToGameSaves.Add(name, gameSaves);
+
+            return true;
+        }
+
+        public void Update()
+        {
+            nameToGameSaves.Clear();
+            foreach(string path in Directory.EnumerateDirectories(rootSavePath, "*", SearchOption.TopDirectoryOnly))
+            {
+                string name = Path.GetRelativePath(rootSavePath, path);
+                nameToGameSaves.TryAdd(name, new GameSaves(name, rootSavePath));
+            }
+        }
+
+        public GameSavesManager(string rootSavePath)
+        {
+            this.rootSavePath = rootSavePath;
+            nameToGameSaves = new Dictionary<string, GameSaves>();
+            Update();
+        }
+
+        public readonly string rootSavePath;
+
+        private readonly Dictionary<string, GameSaves> nameToGameSaves;
+    }
+}
