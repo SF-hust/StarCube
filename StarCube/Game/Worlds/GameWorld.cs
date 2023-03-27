5cb21392406fc23ec939e96a4e3f4cd297df8c71
Zhao Yi <1609957690@qq.com>
2023-03-27 22:11:27 +0800

    修改了 World 命名空间名

--- StarCube/Game/Worlds/GameWorld.cs
+++ StarCube/Game/Worlds/GameWorld.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace StarCube.Game.World
+namespace StarCube.Game.Worlds
 {
-    public class GameWorld
+    public class World
     {
     }
 }
