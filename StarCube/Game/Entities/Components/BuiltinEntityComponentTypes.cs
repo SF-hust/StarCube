c272db68c899af48d1098afd1487a386696f1f3c
Zhao Yi <1609957690@qq.com>
2023-04-20 03:54:42 +0800

    新添了 PlayerComponent

--- StarCube/Game/Entities/Components/BuiltinEntityComponentTypes.cs
+++ StarCube/Game/Entities/Components/BuiltinEntityComponentTypes.cs
@@ -7,12 +7,14 @@ namespace StarCube.Game.Entities.Components
     {
         public static ComponentType<Entity> Transform = new TransformComponent.Type();
         public static ComponentType<Entity> Level = new LevelComponent.Type();
+        public static ComponentType<Entity> Player = new PlayerComponent.Type();
 
         static BuiltinEntityComponentTypes()
         {
             DeferredRegister<ComponentType<Entity>> deferredRegister = new DeferredRegister<ComponentType<Entity>>(BuiltinRegistries.EntityComponentType);
             deferredRegister.Register(Transform);
             deferredRegister.Register(Level);
+            deferredRegister.Register(Player);
         }
     }
 }
---
+++ StarCube/Game/Entities/Components/PlayerComponent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Newtonsoft.Json.Linq;
+
+using LiteDB;
+
+using StarCube.Utility;
+using StarCube.Core.Components;
+
+namespace StarCube.Game.Entities.Components
+{
+    public sealed class PlayerComponent : Component<Entity>
+    {
+        public sealed class Type : EntityComponentType<PlayerComponent>
+        {
+            protected override void StoreTo(PlayerComponent component, BsonDocument bson)
+            {
+                bson.Add("guid", component.guid);
+            }
+
+            protected override bool TryCreateFactoryFrom(JObject json, [NotNullWhen(true)] out Func<PlayerComponent>? factory)
+            {
+                factory = null;
+                return false;
+            }
+
+            protected override bool TryRestoreFrom(BsonDocument bson, [NotNullWhen(true)] out PlayerComponent? component)
+            {
+                if (bson.TryGetGuid("guid", out Guid guid))
+                {
+                    component = new PlayerComponent(guid);
+                    return true;
+                }
+
+                component = null;
+                return false;
+            }
+
+            public Type() : base(StringID.Create(Constants.DEFAULT_NAMESPACE, "player"))
+            {
+            }
+        }
+
+        public PlayerComponent(Guid guid) : base(BuiltinEntityComponentTypes.Player)
+        {
+            this.guid = guid;
+        }
+
+        public readonly Guid guid;
+    }
+}
