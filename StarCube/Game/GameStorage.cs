4fdf18c37d127189d51f6aa879b0f82ea8a7441b
Zhao Yi <1609957690@qq.com>
2023-04-30 23:59:22 +0800

    GameStorage 增加了玩家相关的存储

--- StarCube/Game/GameStorage.cs
+++ StarCube/Game/GameStorage.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 using LiteDB;
 
@@ -14,9 +15,15 @@ namespace StarCube.Game
 
         public const string GameMetaCollectionName = "meta";
 
+        public const string PlayerCollectionName = "player";
+
         public const string NameField = "name";
 
         public const string TotalTickCountField = "tick";
+
+        public const string PositionField = "pos";
+
+        public const string WorldField = "world";
 
         /// <summary>
         /// 读取 ServerGame 的总 tick 时间
@@ -58,6 +65,88 @@ namespace StarCube.Game
             gameMetaCollection.Value.Upsert(meta);
         }
 
+        /// <summary>
+        /// 根据 Guid 读取玩家信息
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="name"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public bool TryLoadPlayer(Guid guid, out string name, out Guid worldGuid, out Vector3 position)
+        {
+            name = string.Empty;
+            worldGuid = Guid.Empty;
+            position = Vector3.Zero;
+            if (!playerCollection.IsValueCreated)
+            {
+                return false;
+            }
+
+            BsonDocument? bson = playerCollection.Value.FindById(guid);
+            if (bson == null)
+            {
+                return false;
+            }
+            name = bson[NameField];
+            worldGuid = bson[WorldField];
+            bson.TryGetVector3(PositionField, out position);
+            return true;
+        }
+
+        /// <summary>
+        /// 根据 name 读取玩家信息
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="guid"></param>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public bool TryLoadPlayer(string name, out Guid guid, out Guid worldGuid, out Vector3 position)
+        {
+            guid = Guid.Empty;
+            worldGuid = Guid.Empty;
+            position = Vector3.Zero;
+            if (!playerCollection.IsValueCreated)
+            {
+                return false;
+            }
+
+            BsonDocument? bson = playerCollection.Value.FindOne(Query.EQ(NameField, name));
+            if (bson == null)
+            {
+                return false;
+            }
+            guid = bson["_id"];
+            worldGuid = bson[WorldField];
+            bson.TryGetVector3(PositionField, out position);
+            return true;
+        }
+
+        /// <summary>
+        /// 保存玩家信息
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <param name="name"></param>
+        /// <param name="position"></param>
+        public void SavePlayer(Guid guid, string name, Guid worldGuid, Vector3 position)
+        {
+            playerCollection.Value.EnsureIndex(NameField);
+            BsonDocument bson = new BsonDocument();
+            bson.Add(NameField, name);
+            bson.Add(WorldField, worldGuid);
+            bson.Add(PositionField, position);
+            playerCollection.Value.Upsert(guid, bson);
+        }
+
+        /// <summary>
+        /// 删除玩家信息
+        /// </summary>
+        /// <param name="guid"></param>
+        /// <returns></returns>
+        public bool DropPlayer(Guid guid)
+        {
+            return playerCollection.Value.Delete(guid);
+        }
+
         public void Dispose()
         {
             if (disposed)
@@ -83,6 +172,8 @@ namespace StarCube.Game
             this.saves = saves;
             gameMetaDatabase = saves.OpenOrCreateDatabase(GameMetaDatabasePath);
             gameMetaCollection = new Lazy<ILiteCollection<BsonDocument>>(() => gameMetaDatabase.Value.GetCollectionAndEnsureIndex(GameMetaCollectionName, BsonAutoId.Int32));
+
+            playerCollection = new Lazy<ILiteCollection<BsonDocument>>(() => gameMetaDatabase.Value.GetCollectionAndEnsureIndex(PlayerCollectionName, BsonAutoId.Guid));
         }
 
         private readonly GameSaves saves;
@@ -90,6 +181,8 @@ namespace StarCube.Game
         private readonly StorageDatabase gameMetaDatabase;
 
         private readonly Lazy<ILiteCollection<BsonDocument>> gameMetaCollection;
+
+        private readonly Lazy<ILiteCollection<BsonDocument>> playerCollection;
 
         private volatile bool disposed = false;
     }
