540d9146ccb0feb43961f0e70b4a8a5872c836f9
Zhao Yi <1609957690@qq.com>
2023-06-16 15:41:28 +0800

    拆分了 ByteBuffer 的接口

---
+++ StarCube/Network/ByteBuffer/IByteBufferReader.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+using LiteDB;
+
+using StarCube.Utility.Math;
+
+namespace StarCube.Network.ByteBuffer
+{
+    internal interface IByteBufferReader
+    {
+        public byte ReadByte();
+
+        public void ReadBytes(Span<byte> bytes);
+
+        private T Read<T>()
+            where T : unmanaged
+        {
+            Span<T> valueSpan = stackalloc T[1];
+            Span<byte> bytes = MemoryMarshal.AsBytes(valueSpan);
+            ReadBytes(bytes);
+            if (BitConverter.IsLittleEndian)
+            {
+                bytes.Reverse();
+            }
+            return valueSpan[0];
+        }
+
+        public short ReadInt16()
+        {
+            return Read<short>();
+        }
+
+        public ushort ReadUInt16()
+        {
+            return Read<ushort>();
+        }
+
+        public int ReadInt32()
+        {
+            return Read<int>();
+        }
+
+        public uint ReadUInt32()
+        {
+            return Read<uint>();
+        }
+
+        public long ReadInt64()
+        {
+            return Read<long>();
+        }
+
+        public ulong ReadUInt64()
+        {
+            return Read<ulong>();
+        }
+
+        public int ReadVarInt32()
+        {
+            uint value = ReadVarUInt32();
+            return BitUtil.ZigZagDecode(value);
+        }
+
+        public uint ReadVarUInt32()
+        {
+            byte b;
+            int count = 0;
+            uint value = 0U;
+            do
+            {
+                b = ReadByte();
+                value |= unchecked((uint)(b & 0x7F) << (7 * count));
+                count++;
+            }
+            while ((b & 0x80) != 0 && count < 5);
+            return value;
+        }
+
+        public long ReadVarInt64()
+        {
+            ulong value = ReadVarUInt64();
+            return BitUtil.ZigZagDecode(value);
+        }
+
+        public ulong ReadVarUInt64()
+        {
+            byte b;
+            int count = 0;
+            ulong value = 0LU;
+            do
+            {
+                b = ReadByte();
+                value |= unchecked((ulong)(b & 0x7F) << (7 * count));
+                count++;
+            }
+            while ((b & 0x80) != 0 && count < 10);
+            return value;
+        }
+
+        public float ReadSingle()
+        {
+            return Read<float>();
+        }
+
+        public double ReadDouble()
+        {
+            return Read<double>();
+        }
+
+        public bool ReadBool()
+        {
+            byte b = ReadByte();
+            return b != 0;
+        }
+
+        public decimal ReadDecimal()
+        {
+            return Read<decimal>();
+        }
+
+        public Guid ReadGuid()
+        {
+            Span<byte> bytes = stackalloc byte[16];
+            ReadBytes(bytes);
+            return new Guid(bytes);
+        }
+
+        public DateTime ReadDateTime()
+        {
+            return DateTime.FromBinary(ReadInt64());
+        }
+
+        public DateTime ReadDateTimeUTC()
+        {
+            return new DateTime(ReadInt64(), DateTimeKind.Utc);
+        }
+
+        public string ReadString()
+        {
+            int length = unchecked((int)ReadVarUInt32());
+            Span<byte> bytes = stackalloc byte[length];
+            ReadBytes(bytes);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public JObject ReadJson()
+        {
+            return JObject.Parse(ReadString());
+        }
+
+        public BsonDocument ReadBson()
+        {
+            int length = (int)ReadVarUInt32();
+            byte[] bytes = new byte[length];
+            ReadBytes(bytes);
+            return BsonSerializer.Deserialize(bytes);
+        }
+    }
+}
---
+++ StarCube/Network/ByteBuffer/IByteBufferWriter.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+using Newtonsoft.Json.Linq;
+
+using LiteDB;
+
+using StarCube.Utility.Math;
+
+namespace StarCube.Network.ByteBuffer
+{
+    internal interface IByteBufferWriter
+    {
+        /// <summary>
+        /// 写入一个字节
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteByte(byte value);
+
+        /// <summary>
+        /// 写入若干个字节
+        /// </summary>
+        /// <param name="bytes"></param>
+        public void WriteBytes(ReadOnlySpan<byte> bytes);
+
+        /// <summary>
+        /// 将 value 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        private void WriteBigEndian<T>(T value)
+            where T : unmanaged
+        {
+            Span<T> valueSpan = stackalloc T[1] { value };
+            Span<byte> bytes = MemoryMarshal.AsBytes(valueSpan);
+            if (BitConverter.IsLittleEndian)
+            {
+                bytes.Reverse();
+            }
+            WriteBytes(bytes);
+        }
+
+        /// <summary>
+        /// 写入 16 位有符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteInt16(short value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 16 位无符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteUInt16(ushort value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 32 位有符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteInt32(int value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 32 位无符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteUInt32(uint value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 64 位有符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteInt64(long value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 64 位无符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteUInt64(ulong value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 以可变长方式写入 32 位有符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteVarInt(int value)
+        {
+            WriteVarUInt(BitUtil.ZigZagEncode(value));
+        }
+
+        /// <summary>
+        /// 以可变长方式写入 32 位无符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteVarUInt(uint value)
+        {
+            Span<byte> bytes = stackalloc byte[5];
+            int length = 1;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = (byte)((value >> (i * 7)) & 0x7FU | 0x80U);
+                if (bytes[i] != 0x80)
+                {
+                    length = i + 1;
+                }
+            }
+            bytes[length - 1] &= 0x7F;
+            WriteBytes(bytes[..length]);
+        }
+
+        /// <summary>
+        /// 以可变长方式写入 64 位有符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteVarInt(long value)
+        {
+            WriteVarUInt(BitUtil.ZigZagEncode(value));
+        }
+
+        /// <summary>
+        /// 以可变长方式写入 64 位无符号整数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteVarUInt(ulong value)
+        {
+            Span<byte> bytes = stackalloc byte[10];
+            int length = 1;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = (byte)((value >> (i * 7)) & 0x7FU | 0x80U);
+                if (bytes[i] != 0x80)
+                {
+                    length = i + 1;
+                }
+            }
+            bytes[length - 1] &= 0x7F;
+            WriteBytes(bytes[..length]);
+        }
+
+        /// <summary>
+        /// 写入 32 位浮点数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteSingle(float value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 64 位浮点数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteDouble(double value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入布尔值
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteBool(bool value)
+        {
+            WriteByte(value ? (byte)1 : (byte)0);
+        }
+
+        /// <summary>
+        /// 写入十进制小数
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteDecimal(decimal value)
+        {
+            WriteBigEndian(value);
+        }
+
+        /// <summary>
+        /// 写入 Guid
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteGuid(Guid value)
+        {
+            Span<byte> bytes = stackalloc byte[16];
+            value.TryWriteBytes(bytes);
+            WriteBytes(bytes);
+        }
+
+        /// <summary>
+        /// 写入本地时间
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteDateTime(DateTime value)
+        {
+            WriteInt64(value.ToBinary());
+        }
+
+        /// <summary>
+        /// 写入 UTC 时间
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteDateTimeUTC(DateTime value)
+        {
+            WriteInt64(value.ToUniversalTime().Ticks);
+        }
+
+        /// <summary>
+        /// 写入字符串
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteString(ReadOnlySpan<char> value)
+        {
+            int length = Encoding.UTF8.GetByteCount(value);
+            Span<byte> bytes = stackalloc byte[length];
+            Encoding.UTF8.GetBytes(value, bytes);
+            WriteVarUInt(unchecked((uint)length));
+            WriteBytes(bytes);
+        }
+
+        /// <summary>
+        /// 写入 Json 文档
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteJson(JObject value)
+        {
+            WriteString(value.ToString());
+        }
+
+        /// <summary>
+        /// 写入 Bson 文档
+        /// </summary>
+        /// <param name="value"></param>
+        public void WriteBson(BsonDocument value)
+        {
+            byte[] bytes = BsonSerializer.Serialize(value);
+            WriteVarUInt((uint)bytes.Length);
+            WriteBytes(bytes);
+        }
+    }
+}
